deed05d2f91ffc986c611d5bfda42db9fa3a076f
SammyObeezy <barasam07@gmail.com>
2024-03-19 23:33:26 +0300

    Project set-up

---
+++ client/src/App.jsx
@@ -0,0 +1,22 @@
+import { Route, Routes } from 'react-router-dom'
+import './App.css'
+import IndexPage from './Pages/indexPages'
+import LoginPage from './Pages/LoginPage'
+import Layout from './Layout'
+import RegisterPage from './Pages/RegisterPage'
+import axios from 'axios'
+
+axios.defaults.baseURL = 'http://localhost:4000'
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Layout />}>
+      <Route index element={<IndexPage />} />
+      <Route path='/login' element={<LoginPage />} />
+      <Route path='/register' element={<RegisterPage />} />
+      </Route>
+    </Routes>
+  )
+}
+
+export default App;
