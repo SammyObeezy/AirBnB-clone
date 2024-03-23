1fd64854383ce44adc7c78568b81485630ed9132
SammyObeezy <barasam07@gmail.com>
2024-03-23 16:32:21 +0300

    This will be the next space

--- api/index.js
+++ api/index.js
@@ -5,6 +5,7 @@ const bcrypt = require('bcryptjs');
 const User = require('./models/User.js');
 const jwt = require('jsonwebtoken');
 const cookieParser = require('cookie-parser')
+const imageDownloader = require('image-downloader');
 require('dotenv').config();
 const app = express();
 
@@ -13,7 +14,7 @@ const jwtSecret = 'gbjhfgbiruihuighuigr';
 
 app.use(express.json());
 app.use(cookieParser());
-
+app.use('/uploads', express.static(__dirname+'/uploads'));
 app.use(cors({
     credentials: true,
     origin: 'http://localhost:5173',
@@ -82,4 +83,15 @@ app.get('/profile', (req,res) => {
 app.post('/logout', (req,res) => {
     res.cookie('token', '').json(true);
 });
+
+app.post('/upload-by-link', async (req,res) => {
+    const {Link} = req.body;
+    const newName = 'photo' + Date.now() + '.jpg';
+    await imageDownloader.image({
+        url: Link,
+        dest: __dirname + '/uploads' + newName,
+    });
+    res.json(newName);
+})
+
 app.listen(4000);
